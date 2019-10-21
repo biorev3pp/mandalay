decc1d7865c59704946efeb44bbe4c4587736573
unknown <ranjan.kaundal@gmail.com>
2019-10-21 22:56:14 +0530

    changes upto features

--- resources/views/admin/features/add_update.blade.php
+++ resources/views/admin/features/add_update.blade.php
@@ -1,42 +1,18 @@
 @extends('layouts.app')
-
 @section('content')
-
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>{{$page_title}}</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="{{url('admin/home')}}">Home</a></li>
-              <li class="breadcrumb-item active">Floors</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">  
-        <div class="col-md-12">
-          <div class="card card-outline card-info">
-            <!-- /.card-header -->
-            <div class="card-body pad">
-              <div class="row">
-                <div class="col-md-6">
-                  Home: <b>{{$floor->home->title}}</b> Floor: <b>{{$floor->title}}</b>
-                </div>
-                <div class="col-md-6">
-                  <a href="{{url()->previous()}}" class="col-md-2 float-right d-inline btn btn-block btn-primary text-white">Back</a>
-                </div>
-              </div>
+<div class="container-fluid">
+   <div class="row">
+      <!-- Area Chart -->
+      <div class="col-xl-12">
+         <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+               <h6 class="m-0 font-weight-bold text-primary">@if($data) Update @else Add New @endif <span class="text-success">Feature</span>
+               </h6>
+               <a href="{{url()->previous()}}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50 pr-2"></i>Back</a>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body" id="new_custom_table">
               @if($data)
                 {{Form::model($data,array('id'=>'floors_form','url'=>url('admin/features/save')))}}
                 {{Form::hidden('record_id',Crypt::encrypt($data->id))}}
@@ -45,43 +21,41 @@
               @else
                 {{Form::open(array('id'=>'floors_form','url'=>url('admin/features/save')))}}
                 {{Form::hidden('floor_id',$floor->id)}}
-              @endif
-              <div class="row">
-                <div class=" form-group col-md-6">
-                  <label for="title">Title</label>
-                  {{Form::text('title',null,['class'=>'form-control','id'=>'title','placeholder'=>'Enter Title'])}}
-                </div>
-              </div>
-              <div class="row">
-                <div class="form-group col-md-6">
-                  <label for="featureimage">Select Floor Plan Image</label>
-                  @if(isset($data->image))
-                    @if($data->image!='' || $data->image!=null)
-                    <div class="py-2 image-preview" style="max-width: 600px;">
-                      <img width="100%" height="auto" src="{{asset('images/features/'.$data->image)}}">
-                    </div>
-                    @endif
-                  @endif
-                  <div class="clearfix"></div>
-                  <div class="custom-file">
-                    {{Form::file('image',['class'=>'custom-file-input image-file','id'=>'featureimage'])}}
-                    <label class="custom-file-label" for="featureimage">@if(!isset($data->image)) Choose file @else {{$data->image}} @endif</label>
+              @endif 
+                  <div class="form-row">
+                     <div class="form-row col-md-8">
+                        <div class="form-group col-md-9">
+                           <label for="inputEmail4">Title</label>
+                           {{Form::text('title',null,['class'=>'form-control','id'=>'title','placeholder'=>'Enter Title'])}}
+                        </div>
+                        <div class="form-group col-md-9">
+                           <div class="form-row">
+                              <div class="form-group">
+                                 <label for="inputEmail4">Upload Picture</label>
+                                 <div class="custom-file">
+                                  {{Form::file('image',['class'=>'custom-file-input image-file', 'id'=>'portfolioimage'])}}
+                                    <label class="custom-file-label" for="validatedCustomFile">@if(!isset($data->image)) Choose file @else {{$data->image}} @endif</label>
+                                 </div>
+                                 @if(isset($data->image))
+                                  @if($data->image!='' || $data->image!=null)
+                                  <div class="col-lg-12 mx-auto p-0 o-hidden">
+                                    <img src="{{asset('images/features/'.$data->image)}}" class="img-thumbnail">
+                                  </div>
+                                  @endif
+                                @endif
+                              </div>
+                           </div>
+                        </div>
+                        <div class="col-lg-12">
+                           <button type="submit" class="btn btn-primary float-left"><i class="fas fa-save fa-sm text-white-50 pr-2"></i>Save</button>
+                        </div>
+                     </div>
                   </div>
-                </div>
-
-                <div class="form-group col-md-12">
-                  <div class="bg-white">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </div>
-                </div>
-              {{Form::close()}}
+               {{Form::close()}}
             </div>
-        </div>
-        <!-- /.col-->
+         </div>
       </div>
-      <!-- ./row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+   </div>
+</div>
+</div>
 @endsection
