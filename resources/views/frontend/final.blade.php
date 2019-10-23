1906105d8f961a6fde61059d3a4ee192a5fa772e
unknown <Administrator@BIOREV.STUDIO>
2019-10-23 16:17:13 +0530

    index and css

--- resources/views/frontend/final.blade.php
+++ resources/views/frontend/final.blade.php
@@ -89,7 +89,7 @@
 
             </div>
 			</div>
-            <div class="col-xl-8 col-lg-7 col-md-6">
+            <div id="final_tab_bar_r">
               <div class="card o-hidden shadow-lg mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">
