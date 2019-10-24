d1d76e016e55edb7b22f4f11a477c4eb163df67c
unknown <Administrator@BIOREV.STUDIO>
2019-10-24 14:52:41 +0530

    css

--- resources/views/include/frontend/header.blade.php
+++ resources/views/include/frontend/header.blade.php
@@ -20,7 +20,7 @@
     rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="{{asset('frontend/css/sb-admin-2.css')}}" rel="stylesheet">
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+   
   <script>
       $(document).click(function (e) {
             $el = $(e.target);
