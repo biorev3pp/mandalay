f35cfdc8aeb60c8e86feacaa407afdaaf6c32382
unknown <ranjan.kaundal@gmail.com>
2019-11-15 01:03:11 +0530

    back url issue on acl settings

--- resources/views/admin/features/acl_settings.blade.php
+++ resources/views/admin/features/acl_settings.blade.php
@@ -9,7 +9,7 @@
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                <h6 class="m-0 font-weight-bold text-primary">ACL <span class="text-success">Settings</span>
                </h6>
-               <a href="{{url()->previous()}}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50 pr-2"></i>Back</a>
+               <a href="{{url('admin/features/list/'.Crypt::encrypt($floor->id))}}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa-arrow-left fa-sm text-white-50 pr-2"></i>Back</a>
             </div>
             <!-- Card Body -->
             <div class="card-body">
